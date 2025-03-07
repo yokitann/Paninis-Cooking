86a3e534273845730ea07029a294826428a03aab
yokitann <yotan3@s.sfusd.edu>
2025-03-07 15:46:19 -0800

    copied files over

---
+++ player.gd
@@ -0,0 +1,8 @@
+extends CharacterBody2D
+
+var speed := 400.0 
+
+func _physics_process(_delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = speed * direction
+	move_and_slide()
