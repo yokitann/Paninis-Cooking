095e20846c10d6ce348fc4ee26e529e9501cb5e0
yokitann <yotan3@s.sfusd.edu>
2025-03-12 13:42:18 -0700

    cont start menu

---
+++ start_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var start_game_button = %StartGameButton
+@onready var title = %Title
+
+
+func _ready():
+	start_game_button.grab_focus()
+
+func _on_start_game_button_pressed():
+	get_tree().change_scene_to_file("res://game.tscn")
+
+func _on_end_game_button_pressed():
+	get_tree().quit()
