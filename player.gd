9d0d00040900e3935a0bd2a4dd444f349f69b18b
yokitann <yotan3@s.sfusd.edu>
2025-03-13 22:19:05 -0700

    accidentally made remy here

--- player.gd
+++ player.gd
@@ -1,4 +1,4 @@
-extends CharacterBody2D
+class_name Player extends CharacterBody2D
 
 var speed := 400.0 
 
---
+++ remy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var player_detected = false 
+
+func _ready() -> void:
+	var detection_area = %Detection
+	detection_area.body_entered.connect(_on_body_entered)
+	detection_area.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node) -> void:
+	if body.name == "Player":  
+		player_detected = true
+
+func _on_body_exited(body: Node) -> void:
+	if body.name == "Player": 
+		player_detected = false
+
+func _unhandled_input(event: InputEvent) -> void:
+	if player_detected and event.is_action_pressed("talk"):
+		get_tree().change_scene_to_file("res://start_menu.tscn")
