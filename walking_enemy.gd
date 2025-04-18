76b403aa9139911350412f7d169f40bcc4dea110
EricCa1 <159198095+EricCa1@users.noreply.github.com>
2025-04-18 15:39:36 -0700

    Fox enemy

--- walking_enemy.gd
+++ walking_enemy.gd
@@ -1,25 +1,19 @@
 extends CharacterBody2D
 
+var direction: Vector2 = Vector2.RIGHT
+var speed: float = 25.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+@onready var ledge_check_right = $LedgeCheckRight
+@onready var ledge_check_left = $LedgeCheckLeft
+@onready var sprite = $AnimatedSprite2D 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	var found_wall: bool = is_on_wall()
+	var found_ledge: bool = not ledge_check_right.is_colliding() or not ledge_check_left.is_colliding()
+	
+	if found_wall or found_ledge:
+		direction *= -1
+		sprite.flip_h = direction.x > 0
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	velocity = direction * speed
 	move_and_slide()
