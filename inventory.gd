ace83ab299c889940365b9ea23d684d79d3e63fd
yokitann <yotan3@s.sfusd.edu>
2025-05-02 15:14:51 -0700

    hot bar done! maybe

--- inventory.gd
+++ inventory.gd
@@ -5,16 +5,13 @@ signal updated
 @export var slots: Array[InventorySlot]
 
 func insert(item: InventoryItem):
-	for slot in slots:
-		if slot.item == item:
-			slot.amount += 1 
-			updated.emit()
-			return
+	var item_slots = slots.filter(func(slot): return slot.item == item && slot.amount < slot.item.max)
+	if !item_slots.is_empty():
+		item_slots[0].amount += 1
+	else:
+		var empty_slots = slots.filter(func(slot): return slot.item == null)
+		if !empty_slots.is_empty():
+			empty_slots[0].item = item 
+			empty_slots[0].amount = 1
 	
-	for i in range(slots.size()):
-		if !slots[i].item:
-			slots[i].item = item
-			slots[i].amount = 1
-			updated.emit()
-			return
-	
+	updated.emit()
